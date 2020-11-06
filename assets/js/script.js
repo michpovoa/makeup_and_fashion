bfd29c2c5bf99a06025e6cb6fb5c6e423e008c70
Michelle Melo Póvoa <69477628+michpovoa@users.noreply.github.com>
2020-11-06 01:23:58 -0300

    subindo repositório

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var frida = document.querySelector("nav section i");
+var menu = document.querySelector("nav section:nth-child(3)");
+var frida = document.querySelector("nav section i");
+var menu = document.querySelector("nav section:nth-child(3)");
+frida.addEventListener('click', function() {
+    menu.classList.toggle('mostrar');
+});
+
